4a21e1f6764c3867a7769fd77e591b71af5109cc
John <John.e.lazaro@gmail.com>
2025-03-11 22:22:45 -0600

    added photo for about page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Container className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Typography variant='h1' className="row-start-2">John Lazaro</Typography>
-      <Typography variant='caption' className="row-start-3 align-top">CST Developer</Typography>
+      <Typography variant='caption' className="row-start-3 align-top">CST Student</Typography>
     </Container>
   )
 }
