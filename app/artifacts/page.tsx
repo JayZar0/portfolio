2131d5a2f10774ea87f582f55d6dbe0bbd239d73
John L <lazaro4958@saskpolytech.ca>
2025-03-13 09:43:32 -0600

    added details about the mobile POS application in the artifacts page.

--- app/artifacts/page.tsx
+++ app/artifacts/page.tsx
@@ -7,14 +7,19 @@ export default function ArtifactsPage() {
   return (
     <Container className='m-2'>
       <Typography variant='h4'>Projects</Typography>
-      <Artifact stack='Java' title='Mobile POS'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        malesuada lacus ex, sit amet blandit leo lobortis eget.
+      <Artifact stack='Java SQLite' title='Mobile POS'>
+        A minimal point of sales system for mobile devices that is capable of storing
+        product inventory and transactions. This was made as a school project for mobile
+        application development for android. There are three different interfaces for the
+        user to use such as the transaction list, the transaction creation screen and the
+        main menu. There was also a plan to create a settings screen which would have been
+        used to added sound to the application.
       </Artifact>
       <Artifact stack='Vue Node Express TypeORM SQLite3' title='Scheduling Application'>
         A barebones employee scheduling app that has many tables in a
         database that can be used to store employees, shifts, and departments. There
         will be authentication for employees who have credentials stored in the database.
+        As of right now the two main login roles are managers and employees
       </Artifact>
       <Artifact stack='Next.js Node RabbitMQ Prisma Postgres' title='Flyer Scanner'>
         This application will take a flyer that a user inputs and then use gemini
