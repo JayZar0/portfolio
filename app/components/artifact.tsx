d35e17118be6617a368059981bfcfc960e1618d3
John <John.e.lazaro@gmail.com>
2025-04-26 22:41:56 -0600

    Added images to projects

--- app/components/artifact.tsx
+++ app/components/artifact.tsx
@@ -1,5 +1,5 @@
 import Typography from "@mui/material/Typography";
-import {CardMedia, Divider} from "@mui/material";
+import {Divider} from "@mui/material";
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
