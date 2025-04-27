33f9fbffcb87c1e54f5387dfc459027e62dd2699
John <John.e.lazaro@gmail.com>
2025-04-26 22:40:45 -0600

    Added images to projects

--- app/artifacts/page.tsx
+++ app/artifacts/page.tsx
@@ -7,22 +7,14 @@ export default function ArtifactsPage() {
   return (
     <Container className='m-2 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <Typography variant='h4' component='h4' margin={5}>Projects</Typography>
-      <Artifact stack='Java SQLite' title='Mobile POS'>
-        A minimal point of sales system for mobile devices that is capable of storing
-        product inventory and transactions. This was made as a school project for mobile
-        application development for android. There are three different interfaces for the
-        user to use such as the transaction list, the transaction creation screen and the
-        main menu. There was also a plan to create a settings screen which would have been
-        used to added sound to the application.
-      </Artifact>
-      <Artifact stack='Vue Express TypeORM SQLite3' title='Scheduling Application'>
+      <Artifact stack='Vue Express TypeORM SQLite3' title='Scheduling Application' image='/scheduler.png'>
         A barebones employee scheduling app that has many tables in a
         database that can be used to store employees, shifts, and departments. There
         will be authentication for employees who have credentials stored in the database.
         As of right now the two main login roles are managers and employees. The managers
         have are able to assign/edit shifts, edit and create employees.
       </Artifact>
-      <Artifact stack='Next.js Node RabbitMQ Prisma Postgres' title='Flyer Scanner'>
+      <Artifact stack='Next.js Node RabbitMQ Prisma Postgres' title='Flyer Scanner' image='/prj5.png'>
         This application will take a flyer that a user inputs and then use gemini
         to scan the raw text in the flyer. Afterwards, the raw text will be sent to
         OpenAI where it will be mapped to an object that will check if the product is
--- app/components/artifact.tsx
+++ app/components/artifact.tsx
@@ -1,5 +1,5 @@
 import Typography from "@mui/material/Typography";
-import {Divider} from "@mui/material";
+import {CardMedia, Divider} from "@mui/material";
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -20,7 +20,7 @@ export default function Artifact({
     // TODO add an image tag that will take the image of the project for displaying
     <Card className="m-5">
       <CardContent>
-        <Image alt="" src={image as string | ''} />
+        <Image width={800} height={800} alt="" src={image as string} className="float-left m-5" />
         <Typography variant='h6' component="h6">{ title }</Typography>
         <Typography variant='caption' component='p' color='gray'>Tech Stack: { stack }</Typography>
         <Divider />
